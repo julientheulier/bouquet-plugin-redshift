ba221410157b2da8f5282f6f596662e056794b9f
Julien Theulier <julien@squidsolutions.com>
2016-04-14 16:25:51 +0200

    T1073: implements renderer

--- src/com/squid/core/jdbc/vendor/redshift/render/RedshiftSkinProvider.java
+++ src/com/squid/core/jdbc/vendor/redshift/render/RedshiftSkinProvider.java
@@ -2,12 +2,12 @@
  * Copyright © Squid Solutions, 2016
  *
  * This file is part of Open Bouquet software.
- *  
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
  * published by the Free Software Foundation (version 3 of the License).
  *
- * There is a special FOSS exception to the terms and conditions of the 
+ * There is a special FOSS exception to the terms and conditions of the
  * licenses as they are applied to this program. See LICENSE.txt in
  * the directory of this program distribution.
  *
@@ -27,6 +27,7 @@ import com.squid.core.database.impl.DataSourceReliable;
 import com.squid.core.database.metadata.IMetadataEngine;
 import com.squid.core.database.model.DatabaseProduct;
 import com.squid.core.domain.extensions.DateOperatorDefinition;
+import com.squid.core.domain.extensions.JSONOperatorDefinition;
 import com.squid.core.domain.operators.RankOperatorDefinition;
 import com.squid.core.jdbc.vendor.redshift.postgresql.render.ANSIZeroIfNullFeatureSupport;
 import com.squid.core.jdbc.vendor.redshift.postgresql.render.PostgresSkinProvider;
@@ -40,74 +41,71 @@ import com.squid.core.sql.render.ISkinPref;
 import com.squid.core.sql.render.SQLSkin;
 import com.squid.core.sql.render.ZeroIfNullFeatureSupport;
 
+public class RedshiftSkinProvider extends PostgresSkinProvider {
 
-public class RedshiftSkinProvider
-extends PostgresSkinProvider
-{
+  private static final ZeroIfNullFeatureSupport zeroIfNull = new ANSIZeroIfNullFeatureSupport();
 
-	private static final ZeroIfNullFeatureSupport zeroIfNull = new ANSIZeroIfNullFeatureSupport();
+  public RedshiftSkinProvider() {
+    super();
+    registerOperatorRender(DateOperatorDefinition.DATE_INTERVAL, new RedshiftDateIntervalOperatorRenderer());
+    registerOperatorRender(RankOperatorDefinition.ROWNUMBER_ID, new RowNumber());
+    registerOperatorRender(JSONOperatorDefinition.JSON_ARRAY_LENGTH, new RedshiftJSONOperatorRenderer());
+    registerOperatorRender(JSONOperatorDefinition.JSON_EXTRACT_FROM_ARRAY, new RedshiftJSONOperatorRenderer());
+    registerOperatorRender(JSONOperatorDefinition.JSON_EXTRACT_PATH_TEXT, new RedshiftJSONOperatorRenderer());
+  }
 
-	public RedshiftSkinProvider() {
-		super();
-		registerOperatorRender(DateOperatorDefinition.DATE_INTERVAL, new RedshiftDateIntervalOperatorRenderer());
-		registerOperatorRender(RankOperatorDefinition.ROWNUMBER_ID, new RowNumber());
+  @Override
+  public double computeAccuracy(DatabaseProduct product) {
+    try {
+      if (product != null) {
+        if (IMetadataEngine.REDSHIFT_NAME.equalsIgnoreCase(product.getProductName())) {
+          return PERFECT_MATCH;
+        } else {
+          return NOT_APPLICABLE;
+        }
+      } else {
+        return NOT_APPLICABLE;
+      }
+    } catch (Exception e) {
+      return NOT_APPLICABLE;
+    }
+  }
 
-	}
+  @Override
+  public SQLSkin createSkin(DatabaseProduct product) {
+    return new RedshiftSQLSkin(this, product);
+  }
 
-	@Override
-	public double computeAccuracy(DatabaseProduct product) {
-		try {
-			if (product!=null){
-				if (IMetadataEngine.REDSHIFT_NAME.equalsIgnoreCase(product.getProductName())) {
-					return PERFECT_MATCH;
-				} else {
-					return NOT_APPLICABLE;
-				}
-			}else{
-				return NOT_APPLICABLE;
-			}
-		} catch (Exception e) {
-			return NOT_APPLICABLE;
-		}
-	}
+  @Override
+  public String getSkinPrefix(DatabaseProduct product) {
+    return "redshift";
+  }
 
-	@Override
-	public SQLSkin createSkin(DatabaseProduct product) {
-		return new RedshiftSQLSkin(this,product);
-	}
+  @Override
+  public ISkinProvider getParentSkinProvider() {
+    return SkinRegistry.INSTANCE.findSkinProvider(PostgresSkinProvider.class);
+  }
 
-	@Override
-	public String getSkinPrefix(DatabaseProduct product) {
-		return "redshift";
-	}
+  @Override
+  public ISkinFeatureSupport getFeatureSupport(DefaultJDBCSkin skin, String featureID) {
+    if (featureID == IGroupingSetSupport.ID) {
+      return IGroupingSetSupport.IS_NOT_SUPPORTED;
+    } else if (featureID == IMetadataForeignKeySupport.ID) {
+      return ISkinFeatureSupport.IS_NOT_SUPPORTED;
+    } else if (featureID == DataSourceReliable.FeatureSupport.AUTOCOMMIT) {
+      return ISkinFeatureSupport.IS_NOT_SUPPORTED;
+    }
 
-	@Override
-	public ISkinProvider getParentSkinProvider() {
-		return SkinRegistry.INSTANCE.findSkinProvider(PostgresSkinProvider.class);
-	}
+    // else
+    return super.getFeatureSupport(skin, featureID);
+  }
 
-
-	@Override
-	public ISkinFeatureSupport getFeatureSupport(DefaultJDBCSkin skin, String featureID) {
-		if (featureID == IGroupingSetSupport.ID) {
-			return IGroupingSetSupport.IS_NOT_SUPPORTED;
-		}
-		else if (featureID == IMetadataForeignKeySupport.ID) {
-			return ISkinFeatureSupport.IS_NOT_SUPPORTED;
-		}else if(featureID == DataSourceReliable.FeatureSupport.AUTOCOMMIT){
-			return ISkinFeatureSupport.IS_NOT_SUPPORTED;
-		}
-
-		//else
-		return super.getFeatureSupport(skin,featureID);
-	}
-	
-	@Override
-	public ISkinPref getPreferences(DefaultJDBCSkin skin, String featureID) {
-		if (featureID == DataSourceReliable.FeatureSupport.ROLLUP) {
-			return ISkinPref.TEMP;
-		}
-		return super.getPreferences(skin, featureID);
-	}
+  @Override
+  public ISkinPref getPreferences(DefaultJDBCSkin skin, String featureID) {
+    if (featureID == DataSourceReliable.FeatureSupport.ROLLUP) {
+      return ISkinPref.TEMP;
+    }
+    return super.getPreferences(skin, featureID);
+  }
 
 }
