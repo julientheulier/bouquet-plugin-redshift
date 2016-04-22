c41a35bfec6e02a23d28d71305818e580f67f756
Julien Theulier <julien@squidsolutions.com>
2016-04-22 15:26:46 +0200

    T1128: declare renderer for shortcuts

--- src/com/squid/core/jdbc/vendor/redshift/postgresql/render/PostgresSkinProvider.java
+++ src/com/squid/core/jdbc/vendor/redshift/postgresql/render/PostgresSkinProvider.java
@@ -32,6 +32,7 @@ import com.squid.core.database.model.DatabaseProduct;
 import com.squid.core.domain.extensions.AddMonthsOperatorDefinition;
 import com.squid.core.domain.extensions.DateOperatorDefinition;
 import com.squid.core.domain.extensions.DateTruncateOperatorDefinition;
+import com.squid.core.domain.extensions.DateTruncateShortcutsOperatorDefinition;
 import com.squid.core.domain.extensions.ExtractOperatorDefinition;
 import com.squid.core.domain.extensions.PosStringOperatorDefinition;
 import com.squid.core.domain.extensions.RegexpOperatorDefinition;
@@ -84,6 +85,11 @@ public class PostgresSkinProvider extends DefaultSkinProvider {
     registerOperatorRender(DateOperatorDefinition.FROM_UNIXTIME, new PostgresDateEpochOperatorRenderer(DateEpochOperatorRenderer.FROM));
     registerOperatorRender(DateOperatorDefinition.TO_UNIXTIME, new PostgresDateEpochOperatorRenderer(DateEpochOperatorRenderer.TO));
     registerOperatorRender(DateTruncateOperatorDefinition.DATE_TRUNCATE, new PostgresDateTruncateOperatorRenderer());
+    registerOperatorRender(DateTruncateShortcutsOperatorDefinition.HOURLY_ID, new PostgresDateTruncateOperatorRenderer());
+    registerOperatorRender(DateTruncateShortcutsOperatorDefinition.DAILY_ID, new PostgresDateTruncateOperatorRenderer());
+    registerOperatorRender(DateTruncateShortcutsOperatorDefinition.WEEKLY_ID, new PostgresDateTruncateOperatorRenderer());
+    registerOperatorRender(DateTruncateShortcutsOperatorDefinition.MONTHLY_ID, new PostgresDateTruncateOperatorRenderer());
+    registerOperatorRender(DateTruncateShortcutsOperatorDefinition.YEARLY_ID, new PostgresDateTruncateOperatorRenderer());
     //
     // See Ticket #1620
     // registerOperatorRender(IntervalOperatorDefinition.INTERVAL_DAY, new PostgresIntervalOperatorRenderer("DAY"));
