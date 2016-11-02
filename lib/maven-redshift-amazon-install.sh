4ee31cced8149e4f58ba640897ec90751da13339
Serge Fantino <serge@squidsolutions.com>
2016-11-02 22:51:54 +0100

    T2163: upgrading to version 1.1.17.1017

--- lib/maven-redshift-amazon-install.sh
+++ lib/maven-redshift-amazon-install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-# now we know why we hate them...
+# install the redshift JDBC driver in maven so we can build the plugin
 #
 BASEDIR=$(dirname $0)
 
-mvn install:install-file -DgroupId=com.amazon -DartifactId=redshift.jdbc41.Driver -Dversion=1.1.13.1013 -Dpackaging=jar -Dfile=${BASEDIR}/RedshiftJDBC41-1.1.13.1013.jar -DgeneratePom=true 
+mvn install:install-file -DgroupId=com.amazon -DartifactId=redshift.jdbc41.Driver -Dversion=1.1.17.1017 -Dpackaging=jar -Dfile=${BASEDIR}/RedshiftJDBC41-1.1.17.1017.jar -DgeneratePom=true 
