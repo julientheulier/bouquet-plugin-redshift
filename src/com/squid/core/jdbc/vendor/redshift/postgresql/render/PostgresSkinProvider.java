b9fa6ecd69dbe36ec7fd27f5dcf6653c68223c31
hoahd <hoa@squidsolutions.com>
2016-11-04 11:58:37 +0100

    T2198 QUARTERLY correct spelling

--- src/com/squid/core/jdbc/vendor/redshift/postgresql/render/PostgresSkinProvider.java
+++ src/com/squid/core/jdbc/vendor/redshift/postgresql/render/PostgresSkinProvider.java
@@ -89,7 +89,7 @@ public class PostgresSkinProvider extends DefaultSkinProvider {
     registerOperatorRender(DateTruncateShortcutsOperatorDefinition.DAILY_ID, new PostgresDateTruncateOperatorRenderer());
     registerOperatorRender(DateTruncateShortcutsOperatorDefinition.WEEKLY_ID, new PostgresDateTruncateOperatorRenderer());
     registerOperatorRender(DateTruncateShortcutsOperatorDefinition.MONTHLY_ID, new PostgresDateTruncateOperatorRenderer());
-    registerOperatorRender(DateTruncateShortcutsOperatorDefinition.QUARTERLY, new PostgresDateTruncateOperatorRenderer());
+    registerOperatorRender(DateTruncateShortcutsOperatorDefinition.QUARTERLY_ID, new PostgresDateTruncateOperatorRenderer());
     registerOperatorRender(DateTruncateShortcutsOperatorDefinition.YEARLY_ID, new PostgresDateTruncateOperatorRenderer());
     //
     // See Ticket #1620
