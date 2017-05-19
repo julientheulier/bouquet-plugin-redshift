31c13dba0a8533f6d638b984f46997fe70787f3c
Julien Theulier <julien@squidsolutions.com>
2017-05-19 13:29:26 +0200

    T3079 Replace VARIANCE by VAR_POP, as VARIANCE is an alias of VAR_SAMP cosmetic

--- src/com/squid/core/jdbc/vendor/redshift/render/RedshiftDateIntervalOperatorRenderer.java
+++ src/com/squid/core/jdbc/vendor/redshift/render/RedshiftDateIntervalOperatorRenderer.java
@@ -2,12 +2,12 @@
  * Copyright © Squid Solutions, 2016
  *
  * This file is part of Open Bouquet software.
- *  
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
  * published by the Free Software Foundation (version 3 of the License).
  *
- * There is a special FOSS exception to the terms and conditions of the 
+ * There is a special FOSS exception to the terms and conditions of the
  * licenses as they are applied to this program. See LICENSE.txt in
  * the directory of this program distribution.
  *
@@ -52,7 +52,7 @@ extends DateIntervalOperatorRenderer
 		if (position==-1) {
 			throw new RuntimeException("The last argument must be a valid period");
 		}
-		String txt = "datediff("+args[2].trim().replaceAll("'", "").toLowerCase()+", ";
+		String txt = "DATEDIFF("+args[2].trim().replaceAll("'", "").toLowerCase()+", ";
 		txt += args[1] + ", " + args[0];
 		txt += ")";
 		return txt;
